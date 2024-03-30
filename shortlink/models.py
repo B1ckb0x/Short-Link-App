6c818656f2f7bbc154c0fecb281044dee10537c2
Emmanuel-As <a.emmanuel0616@gmail.com>
2024-03-30 14:50:52 -0400

    feat: models automation

--- shortlink/models.py
+++ shortlink/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+from django.utils import slugify
 
 # Create your models here.
 
-# Save shortened link
-"""
-name,
-url,
-slug
-# of clicks
-"""
+class Link(models.Model):
+    name = models.CharField(max_length=50, unqiue=True)
+    url = models.URLField()
+    slug = models.SlugField()
+    clicks = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{ self.name } | { self.clicks }"
+    
+    def click(self):
+        self.clicks += 1
+        self.save()
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        
+        return super().save(*args, **kwargs)
