db2c50242bddf8d64df13a812a5d484531fbc048
Emmanuel-As <a.emmanuel0616@gmail.com>
2024-03-30 21:58:47 -0400

    feat: Setup homepage
    Allow user to create links through django forms
    
    Styled homepage for good UI

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party apps
+    'crispy_forms',
+    'crispy_tailwind',
+
     'shortlink',
 ]
 
@@ -123,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Crispy Template Packs
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+CRISPY_TEMPLATE_PACK = "tailwind"
